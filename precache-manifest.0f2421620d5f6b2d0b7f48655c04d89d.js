114528fa46d69006a6ca395ab2c6e0bff208c8b3
Harry Ji <jiharry@hotmail.com>
2022-02-03 14:55:04 -0800

    Updates

---
+++ precache-manifest.0f2421620d5f6b2d0b7f48655c04d89d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ef6d52be7e8b1addfa77b8eaf2cfae",
+    "url": "/react-todolist-local-storage/index.html"
+  },
+  {
+    "revision": "1c3875727f173f8c00d4",
+    "url": "/react-todolist-local-storage/static/js/2.31157228.chunk.js"
+  },
+  {
+    "revision": "52d800a247281e3ca64a",
+    "url": "/react-todolist-local-storage/static/js/main.9e15dd13.chunk.js"
+  },
+  {
+    "revision": "1305da6cd476ec5a4ccb",
+    "url": "/react-todolist-local-storage/static/js/runtime-main.c65b4693.js"
+  }
+]);
